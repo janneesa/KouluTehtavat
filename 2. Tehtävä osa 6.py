f69ffc455655df424846f8a94d30aac1717391c7
janneesa <janne.savinainen@hotmail.com>
2023-08-22 18:37:03 +0300

    2. Tehtävä osa 6: Valmis

---
+++ 2. Tehtävä osa 6.py
@@ -0,0 +1,14 @@
+import random
+
+kolmikko1 = random.randint(0 ,9)
+kolmikko2 = random.randint(0, 9)
+kolmikko3 = random.randint(0, 9)
+
+print(str(kolmikko1)+str(kolmikko2)+str(kolmikko3))
+
+nelikko1 = random.randint(1, 6)
+nelikko2 = random.randint(1, 6)
+nelikko3 = random.randint(1, 6)
+nelikko4 = random.randint(1, 6)
+
+print(str(nelikko1)+str(nelikko2)+str(nelikko3)+str(nelikko4))
