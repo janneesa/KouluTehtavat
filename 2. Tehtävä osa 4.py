b3116ad60dd9960239ac6623ed04cd01f5edcab4
janneesa <janne.savinainen@hotmail.com>
2023-08-22 17:14:15 +0300

    2. Tehtävä osa 4: Valmis

---
+++ 2. Tehtävä osa 4.py
@@ -0,0 +1,12 @@
+luku1 = input('Kerro ensimmäinen luku: ')
+luku2 = input('Kerro toinen luku: ')
+luku3 = input('Kerro kolmas luku: ')
+
+summa = int(luku1) + int(luku2) + int(luku3)
+print('Lukujen summa on: ' + str(summa))
+
+tulo = int(luku1) * int(luku2) * int(luku3)
+print('lukujen tulo on: ' + str(tulo))
+
+keskiarvo = summa / 3
+print('Lukujen keskiarvo on: ' + str(keskiarvo))
