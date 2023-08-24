c03de00d7881c62e542e2b5ce524382ea045d06f
Janne <janneesa@metropolia.fi>
2023-08-24 12:51:24 +0300

    valmis korjattu

--- 3. Valintarakenne/3. tehtävä osa 4.py
+++ 3. Valintarakenne/3. tehtävä osa 4.py
@@ -2,10 +2,8 @@ while True:
 
     vuosi = int(input('Ilmoita vuosi: '))
 
-    if vuosi % 4 == 0:
-        print('Vuosi on karkausvuosi')
+    if vuosi % 4 == 0 and vuosi % 100 == 0 and vuosi % 400 != 0 or vuosi % 4:
+        print('Vuosi ei ole karkausvuosi')
 
-    elif vuosi % 400 == 0 and vuosi % 100 == 0:
-        print('Vuosi on karkausvuosi')
 
-    else: print('Vuosi ei ole karkausvuosi')
+    else: print('Vuosi on karkausvuosi')
