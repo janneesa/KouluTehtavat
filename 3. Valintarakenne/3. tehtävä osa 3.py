7e8447e80387a4144511f497e996ffd559e32216
janneesa <janne.savinainen@hotmail.com>
2023-08-28 18:47:56 +0300

    3.3 Valmis

--- 3. Valintarakenne/3. tehtävä osa 3.py
+++ 3. Valintarakenne/3. tehtävä osa 3.py
@@ -3,13 +3,10 @@ hemo = int(input('ilmoita hemoglobiiniarvosi g/l: '))
 
 if sex.upper() == 'M' and hemo < 134:
     print('hemoglobiiniarvosi on alhainen')
-
 elif sex.upper() == 'M' and hemo <= 195:
     print('hemoglobiiniarvosi on normaali')
-
 elif sex.upper() == 'M' and hemo > 195:
     print('hemoglobiiniarvosi on korkea')
-
 elif sex.upper() == 'F' and hemo < 117:
     print('hemoglobiiniarvosi on alhainen')
 elif sex.upper() == 'F' and hemo <= 175:
@@ -18,5 +15,3 @@ elif sex.upper() == 'F' and hemo > 175:
     print('hemoglobiiniarvosi on korkea')
 else:
     print('virheelliset arvot')
-
-    
