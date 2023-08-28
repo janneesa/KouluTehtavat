77ae6076c81377f3d31bcaaebb7bbc87171c58fe
janneesa <janne.savinainen@hotmail.com>
2023-08-28 18:51:52 +0300

    3.4 Valmis

--- 3. Valintarakenne/3. tehtävä osa 4.py
+++ 3. Valintarakenne/3. tehtävä osa 4.py
@@ -5,5 +5,5 @@ while True:
     if vuosi % 4 == 0 and vuosi % 100 == 0 and vuosi % 400 != 0 or vuosi % 4:
         print('Vuosi ei ole karkausvuosi')
 
-
-    else: print('Vuosi on karkausvuosi')
+    else:
+        print('Vuosi on karkausvuosi')
