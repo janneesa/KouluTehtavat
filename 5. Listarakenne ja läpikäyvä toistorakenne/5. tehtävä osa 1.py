3a2b671a95771662810cc79a66882d3ddea64868
Janne <janneesa@metropolia.fi>
2023-09-05 16:28:58 +0300

    Tarkistukset ja korjattu

--- 5. Listarakenne ja läpikäyvä toistorakenne/5. tehtävä osa 1.py
+++ 5. Listarakenne ja läpikäyvä toistorakenne/5. tehtävä osa 1.py
@@ -3,7 +3,7 @@ import random
 maara = int(input('Montako kuutiota haluat heittää: '))
 summa = 0
 
-for x in range(maara):
+for _ in range(maara):
     luku = random.randint(1, 6)
     summa = summa + luku
 
