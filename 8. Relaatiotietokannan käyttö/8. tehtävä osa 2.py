1a5bac9a2e7620290479514e08ab4a432d8d517f
Janne <janneesa@metropolia.fi>
2023-09-07 10:47:23 +0300

    aloitettu

--- 8. Relaatiotietokannan käyttö/8. tehtävä osa 2.py
+++ 8. Relaatiotietokannan käyttö/8. tehtävä osa 2.py
@@ -12,15 +12,21 @@ yhteys = mysql.connector.connect(
 maakoodi = input('Anna maakoodi: ')
 
 def tyyppihakuri(tyyppi):
-    sql = 'SELECT airport.type FROM airport'
-    sql += f" WHERE airport.type = '{tyyppi}'"
-    print(sql)
+    sql = 'SELECT airport.type FROM airport '
+    sql += f"WHERE airport.iso_country = '{maakoodi}' AND TYPE = '{tyyppi}'"
+    #print(sql)
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
     if kursori.rowcount > 0:
         print(f"{tyyppi} kenttiä on {len(tulos)}")
+    elif kursori.rowcount == 0:
+        print(f'{tyyppi} kenttiä on nolla')
 
 tyyppihakuri("heliport")
 tyyppihakuri("small_airport")
+tyyppihakuri("medium_airport")
+tyyppihakuri('large_airport')
+tyyppihakuri('seaplane_base')
+tyyppihakuri('closed')
 
