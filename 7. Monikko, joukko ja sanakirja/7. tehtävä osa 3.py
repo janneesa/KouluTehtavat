6d1a80405a0cd77ec71fff128be8cf735e751400
Janne <janneesa@metropolia.fi>
2023-09-12 10:16:38 +0300

    valmis

--- 7. Monikko, joukko ja sanakirja/7. tehtävä osa 3.py
+++ 7. Monikko, joukko ja sanakirja/7. tehtävä osa 3.py
@@ -1,24 +1,24 @@
-lentokentat = {'EFHK':'Helsinki'}
+lentokentat = {'EFHK': 'Helsinki'}
 lopetus = 0
 
-def haku(icao): #funktio joka hakee kentän icao koodilla
+def haku(icao):  #funktio joka hakee kentän icao koodilla
     print(f'{icao} on {lentokentat[icao]} airport.')
     return
 
-def add(icao, kentta): #funktio joka lisää icaon ja kentän
+def add(icao, kentta):  #funktio joka lisää icaon ja kentän
     lentokentat[icao] = kentta
     print(f'{icao} : {kentta} lisätty.')
     return
 
 print('Tällä ohjelmalla voit: hakea ja lisätä tietoja lentokentistä järjestelmään.\nAlla on lueteltu käytettävät toiminnot')
-toiminto = input('Haku, Lisäys, Lopeta: ') #määrätään toiminto
+toiminto = input('Haku, Lisäys, Lopeta: ')  #määrätään toiminto
 
 while lopetus < 1:
-    if toiminto.lower() == 'lopeta' or toiminto == '': #lopetetaan tehtävä
+    if toiminto.lower() == 'lopeta' or toiminto == '':  #lopetetaan tehtävä
         lopetus = 1
         print('Ohjelma sammutettu')
 
-    elif toiminto.lower() == 'haku': #haetaan kenttää icao koodilla
+    elif toiminto.lower() == 'haku':  #haetaan kenttää icao koodilla
         ICAO = input('Palaa syöttämällä tyhjä rivi.\nAnna ICAO: ')
         if ICAO == '':
             toiminto = 'paluu'
@@ -27,10 +27,12 @@ while lopetus < 1:
         else:
             haku(ICAO.upper())
 
-    elif toiminto.lower() == 'lisäys': #lisätään icao ja kenttä
+    elif toiminto.lower() == 'lisäys':  #lisätään icao ja kenttä
         ICAO = input('Palaa syöttämällä tyhjä rivi.\nSyötä ICAO: ')
         if ICAO == '':
             toiminto = 'paluu'
+        if toiminto == 'paluu':
+            continue
         KENTTA = input('Palaa syöttämällä tyhjä rivi.\nSyötä lentokentän nimi: ')
         if KENTTA == '':
             toiminto = 'paluu'
