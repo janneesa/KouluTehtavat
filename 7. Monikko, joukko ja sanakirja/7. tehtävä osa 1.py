637e055252a29e293e5fb4e3113ef8d50842184d
Janne <janneesa@metropolia.fi>
2023-09-12 09:39:28 +0300

    valmis

--- 7. Monikko, joukko ja sanakirja/7. tehtävä osa 1.py
+++ 7. Monikko, joukko ja sanakirja/7. tehtävä osa 1.py
@@ -1,27 +1,6 @@
-#tammikuu, helmikuu, maaliskuu, huhtikuu, toukokuu, kesäkuu,
-#heinäkuu, elokuu, syyskuu, lokakuu, marraskuu, joulukuu
-
-#talvi = ['joulukuu', 'tammikuu', 'helmikuu']
-#kevät = ['maaliskuu', 'huhtikuu', 'toukokuu']
-#kesa = ['kesäkuu', 'heinäkuu', 'elokuu']
-#syksy = ['syyskuu', 'lokakuu', 'marraskuu']
-
-talvi = (12, 1, 2)
-kevat = (3, 4, 5)
-kesa = (6, 7, 8)
-syksy = (9, 10, 11)
-
-kuukausi = int(input('Anna kuukausi numerona 1-12: '))
-
-if kuukausi in talvi:
-    print('Talvi')
-elif kuukausi in kevat:
-    print('Kevät')
-elif kuukausi in kesa:
-    print('Kesä')
-elif kuukausi in syksy:
-    print('Syksy')
+vuodenajat = ('talvi', 'talvi', 'kevät', 'kevät', 'kevät', 'kesä', 'kesä', 'kesä', 'syksy', 'syksy', 'syksy', 'talvi')
+kuukausi = int(input('Anna kuukausi numerona: '))
+if kuukausi - 1 not in range(len(vuodenajat)):
+    print('virheellinen kuukausi.')
 else:
-    print('Virheellinen kuukausi.')
-
-
+    print(vuodenajat[kuukausi - 1])
