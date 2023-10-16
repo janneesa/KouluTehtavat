34ce9a874ff63312e1e66e57086a1469fc8a74f8
janneesa <janne.savinainen@hotmail.com>
2023-10-16 14:49:21 +0300

    web projekti

--- Test/codewars.py
+++
@@ -1,8 +0,0 @@
-def same_case(a, b):
-    if a.isalpha() == False or b.isalpha() == False:
-        return -1
-    elif (a.isupper() and b.isupper()) or (a.islower() and b.islower()):
-        return 1
-    elif a.isalpha() and b.isalpha():
-        return 0
-    pass
