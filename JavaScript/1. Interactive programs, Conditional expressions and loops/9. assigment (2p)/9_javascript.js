8983501c78cf019d161445e3e2637571236b320f
Janne <janneesa@metropolia.fi>
2023-11-07 09:01:52 +0200

    9 valmis

--- JavaScript/1. Interactive programs, Conditional expressions and loops/9. assigment (2p)/9_javascript.js
+++ JavaScript/1. Interactive programs, Conditional expressions and loops/9. assigment (2p)/9_javascript.js
@@ -1,9 +1,22 @@
-const number = parseInt(prompt('Input a number.'))
-for (let i = 2; number; i++ ) {
-    if (number % i === 0) {
-        document.querySelector('#target').innerHTML = `${number} is not a prime number.`;
-        console.log(i)
-        break
+const number = parseInt(prompt('Input a number.'));
+
+let isPrime = true;
+
+if (number === 1) {
+    isPrime = false;
+} else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
     }
 }
+
+if (isPrime) {
+    document.querySelector('#target').innerHTML = `${number} is a prime number.`;
+} else {
+    document.querySelector('#target').innerHTML = `${number} is not a prime number.`;
+}
+
 
